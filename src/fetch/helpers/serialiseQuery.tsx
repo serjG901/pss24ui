b56c6b809a774feafef29006c1c70a16efd90fef
serjG901 <70537823+serjG901@users.noreply.github.com>
2024-05-22 00:47:18 +0300

    Update serialiseQuery.tsx

--- src/fetch/helpers/serialiseQuery.tsx
+++ src/fetch/helpers/serialiseQuery.tsx
@@ -3,7 +3,7 @@ export default function serialiseQuery(obj: {
 }) {
     const arr: string[] = [];
     for (const [key, value] of Object.entries(obj)) {
-        if (value) {
+        if (value || value == 0) {
             if (Array.isArray(value)) {
                 value.forEach((v, index) => arr.push(`${key}[${index}]=${v}`));
             } else {
