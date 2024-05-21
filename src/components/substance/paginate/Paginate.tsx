892bc04d3246b1500b8bbeb1554c6c903f29f53e
serjG901 <70537823+serjG901@users.noreply.github.com>
2024-05-22 00:47:10 +0300

    Update Paginate.tsx

--- src/components/substance/paginate/Paginate.tsx
+++ src/components/substance/paginate/Paginate.tsx
@@ -1,6 +1,7 @@
 import "./style.css";
 import PaginatePageButton from "../../molecul/paginate-page-button";
 import PaginateDots from "../../molecul/paginate-dots";
+import calculatePaginationRenderPage from "../../../clearFunctions/calculatePaginationRenderPage";
 
 interface IPaginate {
   pageActive: number;
@@ -13,56 +14,6 @@ interface IPaginate {
   paginateTitlePrevious: string;
   paginateTitlePage: string;
   paginateTitleNext: string;
-}
-
-function calculatePagination(
-  pageActiveRender: number,
-  pages: number
-): (number | string)[] {
-  let renderPages: (number | string)[] = [];
-  switch (pageActiveRender) {
-    case 1:
-      renderPages = [
-        [1, 2],
-        [1, 2, 3],
-        [1, 2, "..", pages],
-      ][pages > 3 ? 2 : pages - 2];
-      break;
-    case 2:
-      renderPages = [
-        [1, 2],
-        [1, 2, 3],
-        [1, 2, 3, 4],
-        [1, 2, 3, "..", pages],
-      ][pages > 4 ? 3 : pages - 2];
-      break;
-
-    case 3:
-      renderPages = [
-        [1, 2, 3],
-        [1, 2, 3, 4],
-        [1, 2, 3, 4, 5],
-        [1, 2, 3, 4, "..", pages],
-      ][pages > 5 ? 3 : pages - 3];
-      break;
-
-    default:
-      renderPages = [
-        [1, "..", pages - 1, pages],
-        [1, "..", pages - 2, pages - 1, pages],
-        [1, "..", pages - 3, pages - 2, pages - 1, pages],
-        [
-          1,
-          "..",
-          pageActiveRender - 1,
-          pageActiveRender,
-          pageActiveRender + 1,
-          "..",
-          pages,
-        ],
-      ][pages - pageActiveRender > 3 ? 3 : pages - pageActiveRender];
-  }
-  return renderPages;
 }
 
 export default function Paginate({
@@ -81,7 +32,7 @@ export default function Paginate({
 
   if (pages < 2) return null;
   const pageActiveRender = pageActive;
-  const renderPages = calculatePagination(pageActiveRender, pages);
+  const renderPages = calculatePaginationRenderPage(pageActiveRender, pages);
   return (
     <div className="paginate">
       <PaginatePageButton
